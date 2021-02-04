619a2495f640a5936475bc7ef3f5accb825a3460
Wilson Alves <wlsn.ads@gmail.com>
2021-02-03 21:27:39 -0300

    generics, set, dictionary

---
+++ ExercicioDicionario/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace ExercicioDicionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    Dictionary<string, int> dictionary = new Dictionary<string, int>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] votesCandidates = sr.ReadLine().Split(',');
+                        string candidate = votesCandidates[0];
+                        int votes = int.Parse(votesCandidates[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+                    foreach (var item in dictionary)
+                    {
+                        Console.WriteLine(item.Key + " " + item.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine(e.Message); ;
+            }
+        }
+    }
+}
---
+++ ExercicioSet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using ExercicioSet.Entities;
+namespace ExercicioSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream) 
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord(name, instant));
+                        Console.WriteLine(name, instant);
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
