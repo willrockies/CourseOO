bfdbab34330aa4d80fdabc34504d0e9d78d2d53d
willrockies <wlsn.ads@gmail.com>
2022-05-30 09:45:59 -0300

    Seção 9 - enum e composições

---
+++ secao9EnumeracaoComposicao/Program.cs
@@ -0,0 +1,40 @@
+﻿using secao9EnumeracaoComposicao.Entities;
+using secao9EnumeracaoComposicao.Entities.Enums;
+using System;
+
+namespace secao9EnumeracaoComposicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Aula 126*/
+
+            Console.WriteLine("Aula 126");
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment,
+            };
+            Console.WriteLine(order);
+
+            // Conversões
+            OrderStatus osInt = (OrderStatus)2;
+            Console.WriteLine(osInt);
+            int os1 = (int)OrderStatus.Processing;
+            Console.WriteLine("convertido para inteiro:" + os1);
+
+            // enum - string
+            string osString = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(osString);
+
+            OrderStatus osEnum = Enum.Parse<OrderStatus>("Delivered");
+            //OrderStatus osEnumNonExist = Enum.Parse<OrderStatus>("Cancelled");
+            Console.WriteLine(osEnum);
+
+            Console.WriteLine("-------------------------------");
+
+        }
+    }
+}
